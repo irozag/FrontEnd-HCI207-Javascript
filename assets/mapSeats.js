0b2e80c338885158ca05acb0c90afb13874ed2c2
irozg <irozg@yahoo.com>
2021-03-29 02:05:28 +0300

    add reservation map

---
+++ assets/mapSeats.js
@@ -0,0 +1,17 @@
+const map = document.querySelector("#map");
+let seat = "<tbody>";
+let counter = 0;
+
+for (i=1; i<50; i++) {
+    seat += "<tr>"
+    counter++;
+    for (z=0; z<50; z++) {
+        seat += "<td class='seat' title=" + counter + "></td>"
+        counter++;
+    }
+    seat += "</tr>"
+}
+
+seat += "</tbody>"
+
+map.innerHTML = seat;
