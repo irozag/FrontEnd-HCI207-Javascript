cf791fed9c0ee8810d90901de21c7a4e8375c58d
irozg <irozg@yahoo.com>
2021-04-06 00:18:33 +0300

    days of the month

--- assets/calendar.js
+++ assets/calendar.js
@@ -3,20 +3,38 @@ let play = Object.keys(shows);
 let calendar = document.querySelector("#calendar");
 
 function thisMonth() {
-    let month = new Array("ΙΑΝΟΥΑΡΙΟΣ","ΦΕΒΡΟΥΑΡΙΟΣ", "ΜΑΡΤΙΟΣ", "ΑΠΡΙΛΙΟΣ", "ΜΑΙΟΣ", "ΙΟΥΝΙΟΣ","ΙΟΥΛΙΟΣ","ΑΥΓΟΥΣΤΟΣ","ΣΕΠΤΕΜΒΡΙΟΣ","ΟΚΤΟΒΡΙΟΣ","ΝΟΕΜΒΡΙΟΣ","ΔΕΚΕΜΒΡΙΟΣ");
-    let d = new Date();
-    let n = d.getMonth();
-    let h2 = calendar.querySelector("h2").innerHTML = month[d.getMonth()];
-   // console.log(n);
+    let months = new Array("ΙΑΝΟΥΑΡΙΟΣ","ΦΕΒΡΟΥΑΡΙΟΣ", "ΜΑΡΤΙΟΣ", "ΑΠΡΙΛΙΟΣ", "ΜΑΙΟΣ", "ΙΟΥΝΙΟΣ","ΙΟΥΛΙΟΣ","ΑΥΓΟΥΣΤΟΣ","ΣΕΠΤΕΜΒΡΙΟΣ","ΟΚΤΟΒΡΙΟΣ","ΝΟΕΜΒΡΙΟΣ","ΔΕΚΕΜΒΡΙΟΣ");
+    let date = new Date();
+    let thisDay = date.getDay();
+    let thisMonth = date.getMonth();
+    let thisYear = date.getFullYear();
 
-//    let daysNumber = daysInMonth(n, 2021);
-//        console.log(daysNumber);
+    //print the title
+    let h2 = calendar.querySelector("h2").innerHTML = months[thisMonth];
+
+   // calculate the days of this month
+    let daysM = function(month,year) {
+      return new Date(year, month, 0).getDate();
+   };
+   thisMonth++;
+   let daysInMonth = daysM(thisMonth, thisYear);
+
+
+   
+   console.log(thisDay);
+   console.log("this month is : " + thisMonth);
+   console.log("this month has : " + daysInMonth);
+ 
+
+ 
   }
 
   thisMonth();
 
-play.forEach(function(play){
-    let dates = shows[play].dates;
-    // console.log(dates);
+  
 
-});
+/* play.forEach(function(play){
+    let dates = shows[play].dates;
+    // console.log(dates); 
+
+});*/
