939d624880fedcf378551c5a67595fa670ab1dee
irozg <irozg@yahoo.com>
2021-04-17 11:41:58 +0300

    correct details in play.html

--- assets/play.js
+++ assets/play.js
@@ -1,4 +1,4 @@
-const item =  window.location.href.split("?")[1];
+const item = window.location.href.split("?")[1];
 const play = shows[item];
 console.log(item);
 
@@ -18,7 +18,7 @@ console.log(item);
 // };
 
 document.querySelector(".room").innerHTML = play.room;
-document.querySelector(".title").innerHTML = play.name; 
+document.querySelector(".title").innerHTML = play.name;
 document.querySelector(".composer").innerHTML = play.composer;
 document.querySelector(".dates").innerHTML = play.mounth;
 document.querySelector(".dates").innerHTML = play.mounth;
@@ -27,19 +27,99 @@ document.querySelector(".description").innerHTML = play.description;
 document.querySelector(".feature-photo").style.backgroundImage = "url(" + play.photo + ")";
 
 const crew = play['crew']
-let details = document.querySelectorAll(".details");
 
-// console.log(details);
 
-// let elems = document.querySelectorAll('select option:checked')
-// let values = Array.prototype.map.call(elems, function(obj) {
-//   return obj.value
-// });
 
-for ( i=0; i<details.length; i++ ){
-    for ( person in crew) {
-        console.log(person);
+for (person in crew) {
+    //     if(crew.person != undefined) 
+    // console.log(details[i]);
+    switch (person) {
+        case "conductor":
+            text = "Μαέστρος";
+            break;
+        case "director":
+            text = "Σκηνοθέτης";
+            break;
+        case "sets":
+            text = "Σετς";
+            break;
+        case "costumes":
+            text = "Κοστούμια";
+            break;
+        case "lighting designer":
+            text = "Σχεδιασμός φωτισμού";
+            break;
+        case "chorus master":
+            text = "Χωροδός";
+            break;
+        case "associate-director":
+            text = "Βοηθός Σκηνοθέτη";
+            break;
+        case "choreographer":
+            text = "Χορογράφος";
+            break;
+        case "programmer video":
+            text = "Προγραμματισμός βίντεο";
+            break;
+        case "video designer":
+            text = "Σχεδιασμός βίντεο";
+            break;
+        case "video projection designer":
+            text = "Σχεδιασμός βιντεοπροβολών";
+            break;
+        default:
+            text = " ";
     }
+    let div = document.createElement("div");
+    div.classList.add("details");
+    div.innerHTML= text;
+    let span = document.createElement("span");
+    span.id = person;
+    span.innerHTML = crew[person];
+    div.appendChild(span);
+    let crewdiv = document.querySelector(".crew");
+    crewdiv.appendChild(div);
+
+}
+
+
+
+var text;
+let details = document.querySelectorAll(".details");
+for (i = 0; i < details.length; i++) {
+
+}
+
+
+
+
+
+
+
+// document.querySelector("#conductor").innerHTML = crew.conductor;
+// document.querySelector("#director").innerHTML = crew.director;
+// document.querySelector("#associate").innerHTML = crew['associate director'];
+// document.querySelector("#sets").innerHTML = crew.sets;
+// document.querySelector("#costumes").innerHTML = crew.costumes;
+// document.querySelector("#choreographer").innerHTML = crew.choreographer;
+// document.querySelector("#lighting").innerHTML = crew['lighting designer'];
+// //  document.querySelector("#video").innerHTML = crew[/(video)*(designer)/];
+// document.querySelector("#programmer").innerHTML = crew['programmer video'];
+// document.querySelector("#chorus").innerHTML = crew['chorus master'];
+// document.querySelector("#chorus").innerHTML = crew['chorus master'];
+
+
+
+    // <div class="details">Μαέστρος <span id="conductor"></span></div> 
+    // <div class="details">Σκηνοθέτης <span id="director"></span></div> 
+    // <div class="details">Βοηθός Σκηνοθέτη <span id="associate"></span></div> 
+    // <div class="details">σετ <span id="sets"></span></div> 
+    // <div class="details">Κοστούμια <span id="costumes"></span></div> 
+    // <div class="details">Χορογράφος <span id="choreographer"></span></div> 
+    // <div class="details">Σχεδιασμός φωτισμού <span id="lighting"></span></div> 
+    // <div class="details">Σχεδιασμός βίντεο <span id="video"></span></div> 
+    // <div class="details">Προγραμματισμός βίντεο <span id="programmer"></span></div> 
+    // <div class="details">Χωροδός<span id="chorus"></span></div> 
     // console.log (element);
     // let div = document.createElement('div');
     // div.classList.add('details');
@@ -47,9 +127,9 @@ for ( i=0; i<details.length; i++ ){
     // span.innerHTML = crew[element];
     // div.appendChild(span);
     // document.querySelector(".overlay").appendChild(div);
-  
-  
-}
+
+
+
 // document.querySelector("#conductor").innerHTML = crew.conductor;
 // document.querySelector("#director").innerHTML = crew.director;
 // document.querySelector("#associate").innerHTML = crew['associate director'];
